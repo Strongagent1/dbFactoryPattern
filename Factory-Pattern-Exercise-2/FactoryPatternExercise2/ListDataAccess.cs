f77e7351c4ab1b3478b4e9211c510cf78850bd5a
Kyle Jenne <strongagent1@gmail.com>
2023-01-18 12:54:12 -0500

    DataBase factory exercise for KJ

---
+++ Factory-Pattern-Exercise-2/FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public void SaveData()
+        {
+            Console.WriteLine("Saving data from the List database.");
+        }
+
+        List<Product> IDataAccess.LoadData()
+        {
+            Console.WriteLine("Reading data from the List database.......");
+            Thread.Sleep(3000);
+            Console.WriteLine();
+
+            return new List<Product>()
+            {
+                new Product() { Name = "Samsung UHD Monitor", Price = 499.99 },
+                new Product() { Name = "Dell XPS mouse", Price = 19.99 },
+                new Product() { Name = "Samsung Wireless Keyboard", Price = 79.99 },
+                new Product() { Name = "PS5", Price = 499.99 },
+                new Product() { Name = "Clean Code", Price = 14.99 },
+                new Product() { Name = "Gaming Chair", Price = 299.99 }
+
+
+            };
+        }
+    }
+}
---
+++ Factory-Pattern-Exercise-2/FactoryPatternExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FactoryPatternExercise2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please tell me the type of database you would like to" +
+                " access. You may select from the \'MySQL\', \'Mongo\', or \'List\' databases.");
+
+            var databaseType = Console.ReadLine().ToLower();
+
+            IDataAccess dataType = DataAccessFactory.GetDataAccess(databaseType);
+            List<Product> products = dataType.LoadData();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine($"Product Name: {product.Name} and the price is \'${product.Price}\'.");
+                Console.WriteLine();
+            }
+
+            dataType.SaveData();
+
+        }
+    }
+}
